eeba1e230059b72e87e3c29d03e1799842f03304
FADHILI-Josue <fadhilijosue27@gmail.com>
2023-06-18 21:58:10 +0200

    refresh and building

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -17,7 +17,8 @@ const UserAuthForm: FC<UserAuthFormProps> = ({className, ...props}) => {
     const loginwithgoogle = async () => {
         setIsLoading(true);
         try {
-            await signIn('google')
+            // await signIn('google')
+            throw Error('hello');
         } catch (error) {
             toast({
                 title: 'there was a problem',
