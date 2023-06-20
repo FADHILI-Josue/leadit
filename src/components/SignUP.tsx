1537964ab2a541585217ae784ece2093e882576e
BillBenon <DushimeBillBenon@gmail.com>
2023-06-20 10:50:37 +0200

    adding interception

--- src/components/SignUP.tsx
+++ src/components/SignUP.tsx
@@ -7,21 +7,21 @@ interface SignInProps{
   
 }
 
-const SignIn: FC<SignInProps> = () => {
+const SignUp: FC<SignInProps> = () => {
   return <div className='container mx-auto w-full flex flex-col justify-center space-y-6 sm:w-[400px]'>
     <div className="flex flex-col space-y-2 text-center">
         <Icons.logo className='mx-auto h-6 w-6'/>
-        <h1 className='text-2xl font-semibold tracking-tight'>Welcome Back</h1>
+        <h1 className='text-2xl font-semibold tracking-tight'>Welcome to leadit</h1>
         <p className='text-sm max-w-xs mx-auto'>
             By continuing, you are setting up a reddit account and agree to our user Agreement and our Privacy Policy
         </p>
         {/* sign in form */}
         <UserAuthForm />
         <p className='px-8 text-center text-sm text-zinc-700'>
-            New to Leadit? {' '} <Link href={'/sign-up'} className='hover:text-zinc-400 text-sm underline underline-offset-4'>Sign Up</Link>
+            Already have an account? {' '} <Link href={'/sign-in'} className='hover:text-zinc-400 text-sm underline underline-offset-4'>Sign In</Link>
         </p>
     </div>
   </div>
 }
 
-export default SignIn
+export default SignUp
